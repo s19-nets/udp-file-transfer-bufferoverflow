ae5c6f5f7e7aa8be6a9a3c955caddc2168e99da9
Ederrod <ederrod21@gmail.com>
2019-03-07 09:24:22 -0700

    Adding a .gitignore and cleaning up some todos

--- stopWait/server/server.py
+++ stopWait/server/server.py
@@ -30,6 +30,7 @@ except:
 def idle_handler(sock, client, msg): 
     ''' Rest our previous client variables and our tires '''
     global tries,filehelper
+
     tries = 0 
     filehelper.reset()
     print("Waiting for a client to connect...")
@@ -48,8 +49,6 @@ def wait_handler(sock, client, msg):
     return (client,msg)
 
 def get_handler(sock, client, msg): 
-    ''' Check if our data has been prepared other wise go on 
-        and send data to client '''
     global statemachine,filehelper
 
     if msg[:3] == "GET": 
@@ -68,19 +67,16 @@ def get_handler(sock, client, msg):
     if segment != -1: 
         segment = str(segnum) + ":" + segment.decode()
     else: 
-        # TODO: change the file end message to "END:<empty>"Sanchez
         segment = str('END') + ":" + " "
     print("Send: %s"%segment)
     sock.sendto(segment.encode(), client)
     statemachine.on_event({'event':'msg_sent','msg':msg})
     return (client,segment)
 
-# TODO: put request needs to be handled 
 def put_handler(sock, client, msg): 
     global statemachine, filehelper
     
     if msg[:3] == "PUT": 
-        # TODO: check if file exists
         fname = "files/" + msg[4:]
         if filehelper.fileexists(fname): 
             print("file already exists")
@@ -100,13 +96,8 @@ def put_handler(sock, client, msg):
         statemachine.on_event({'event':'msg_sent','msg':sendmsg})
         return (client,sendmsg)
 
-        
-
-
-
 # TODO: add a state where if an error happens that state machine 
 #       cant handle, the error state will log that error
-
 
 class FileHelper(object):
     def __init__(self):
@@ -140,8 +131,6 @@ class FileHelper(object):
         self.filename = None
         self.splitedfile = {}
 
-
-
 filehelper = FileHelper()
 
 server_socket = socket(AF_INET, SOCK_DGRAM)
@@ -153,7 +142,6 @@ write_set = set()
 error_set = set([server_socket])
 
 statemachine = ServerStateMachine()
-''' TODO: put state handler '''
 statehandler = {}
 statehandler['IdleState'] = idle_handler
 statehandler['WaitState'] = wait_handler

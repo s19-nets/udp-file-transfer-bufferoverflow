042e79db796a5017e538db292b7d470ee7f2812f
bsanc114 <bsanchez12@miners.utep.edu>
2019-03-07 23:52:22 -0500

    try 1 at client

--- stopWait/client/client_states.py
+++ stopWait/client/client_states.py
@@ -2,7 +2,28 @@
 
 #TODO: create client states based on state machine diagram 
 #TODO: update each state to the next state based on the event that has occured
+class IdleState(State):
+    def on_event(self,event):
+        if event['event'] == 'msg_recv':
+            if event['msg'] == "GET":
+                return GetState()
+            else:
+                return PutState()
+        return self
 
+class WaitState(State):
+    def on_event(self,event):
+        if event['event'] == 'msg_recv':
+            if event['msg'] == "ACK":
+                return GetState()
+            elif event['msg'] == 'msg_send':
+                return PutState()
+            else return self
+
+        elif event['event'] == 'err_to':
+            return IdleState()
+        else:
+            return self
 
 class GetState(State):
     def on_event(self,event):

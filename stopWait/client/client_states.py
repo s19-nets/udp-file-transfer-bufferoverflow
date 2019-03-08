ef0017499644bf912cde6dbd7dc71af8c49c177a
bsanc114 <bsanchez12@miners.utep.edu>
2019-03-07 23:33:15 -0500

    Get and put for client state machine

--- stopWait/client/client_states.py
+++ stopWait/client/client_states.py
@@ -3,3 +3,17 @@
 #TODO: create client states based on state machine diagram 
 #TODO: update each state to the next state based on the event that has occured
 
+
+class GetState(State):
+    def on_event(self,event):
+        if event['event'] == 'msg_send'
+        return WaitState()
+    return self
+
+class PutState():
+    def on_event(self,event):
+        if event['event'] == 'ACK'
+        return WaitState()
+    return self
+
+
